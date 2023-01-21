c17678a119fc8b2bb8b7decef197aeac5641878e
Daniel Porto Rojas <dporto@bancolombia.com.co>
2023-01-21 09:23:18 -0500

    feat: comentarios de codigo

--- microservicioB/src/services/weatherForecast.service.js
+++ microservicioB/src/services/weatherForecast.service.js
@@ -7,6 +7,10 @@ const getForecastWeatherByCity = async (city, state, countryCode) => {
   try {
     const { lat, lon } = await convertCityToCoordinates(city, state, countryCode);
     const url = `${WEATHER_FORECAST_API_URL}?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    /*Las consultas a servicios externos como este llamado de axios, en el caso de Node.js se manejan como no bloqueantes,
+      ya que por la naturaleza del lenguaje, este continua con su flujo y espera a que un evento le notifique la respuesta
+      de esta consulta
+    */
     const result = await axios.get(url,
       {
         headers: {
